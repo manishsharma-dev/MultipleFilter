92cfee9764c9d6c0e04cfe42379e1e13c97c9fae
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2020-02-17 12:25:19 +0530

    Create multipleFilterPipe.ts

---
+++ multipleFilterPipe.ts
@@ -0,0 +1,10 @@
+export class FilterPipe implements PipeTransform {
+        transform(items: Array<any>, filter: {[key: string]: any }): Array<any> {
+            return items.filter(item => {
+                let notMatchingField = Object.keys(filter)
+                                             .find(key => item[key] !== filter[key]);
+
+                return !notMatchingField; // true if matches all fields
+            });
+        }
+    }
